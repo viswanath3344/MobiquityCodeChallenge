539054a66ac52abfe3426b30965b9d6c63b308ec
Viswanath <viswanath3344@gmail.com>
2021-02-27 09:26:56 +0530

    Initial Commit

---
+++ MobiquityCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobiquityCodeChallenge
+//
+//  Created by Apple on 27/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
